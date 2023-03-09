dee83b51af2895a16c4f0d0e7662c5478e05e757
asoul <Asoul@asoul_12>
2023-03-09 20:59:30 +0800

    fix:Fix the feed interface so that it can display the likes information correctly

--- controller/feed.go
+++ controller/feed.go
@@ -2,7 +2,6 @@ package controller
 
 import (
 	"context"
-	"fmt"
 	"github.com/cloudwego/hertz/pkg/app"
 	"github.com/sirupsen/logrus"
 	"net/http"
@@ -13,12 +12,15 @@ import (
 )
 
 type FeedService struct {
-	videoRepo repository.VideoRepo
-	userRepo  repository.UserRepo
+	videoRepo    repository.VideoRepo
+	userRepo     repository.UserRepo
+	relationRepo repository.RelationRepo
+	favoriteRepo repository.FavoriteRepo
 }
 
 func (feedService *FeedService) Feed(ctx context.Context, req *app.RequestContext) {
 	token := req.Query("token")
+	userId := req.GetInt64("userId")
 	latestTime := req.Query("latest_time")
 	isLogin := true
 	reqTime := time.Now().UnixMilli()
@@ -27,7 +29,6 @@ func (feedService *FeedService) Feed(ctx context.Context, req *app.RequestContex
 	if len(token) == 0 {
 		isLogin = false
 	}
-	fmt.Println(isLogin)
 	// 首次加载feed流
 	if len(latestTime) != 0 && latestTime != "0" {
 		reqTime, err = strconv.ParseInt(latestTime, 10, 64)
@@ -42,6 +43,7 @@ func (feedService *FeedService) Feed(ctx context.Context, req *app.RequestContex
 	}
 	// repo 根据时间戳加载视频
 	videoList, err := feedService.videoRepo.GetFeedList(reqTime)
+	// 视频库无视频 重新加载
 	if err == nil && len(videoList) == 0 {
 		reqTime = time.Now().UnixMilli()
 		videoList, err = feedService.videoRepo.GetFeedList(reqTime)
@@ -56,14 +58,15 @@ func (feedService *FeedService) Feed(ctx context.Context, req *app.RequestContex
 
 	// 拼装dto对象 1.加载该视频的作者信息 （用户是否关注该作者） 2.判断该视频是否被点赞
 	var videosList []dto.Video
-	for _, v := range videoList {
-		author, err := userRepo.FindUserByUserId(v.Author)
-		if err != nil {
-			req.JSON(http.StatusOK, dto.BaseResp{
-				StatusCode: 1,
-				StatusMsg:  "加载视频作者信息失败",
-			})
-			return
+	for _, video := range videoList {
+		author, _ := userRepo.FindUserByUserId(video.Author)
+		// 判断使用者与视频作者关系
+		// 判断使用者是否点赞视频
+		isFollow := false
+		isFavorite := false
+		if isLogin {
+			isFollow, _ = feedService.relationRepo.CheckFollow(userId, video.Author)
+			isFavorite, _ = feedService.favoriteRepo.CheckFavorite(userId, video.ID)
 		}
 		video := dto.Video{
 			Author: dto.User{
@@ -73,19 +76,19 @@ func (feedService *FeedService) Feed(ctx context.Context, req *app.RequestContex
 				FollowCount:     author.FollowCount,
 				FollowerCount:   author.FollowerCount,
 				ID:              author.ID,
-				IsFollow:        false,
+				IsFollow:        isFollow,
 				Name:            author.Name,
 				Signature:       author.Signature,
 				TotalFavorited:  author.TotalFavorited,
 				WorkCount:       author.WorkCount,
 			},
-			CommentCount:  v.CommentCount,
-			CoverURL:      v.CoverUrl,
-			FavoriteCount: v.FavoriteCount,
-			ID:            v.ID,
-			IsFavorite:    false,
-			PlayURL:       v.PlayUrl,
-			Title:         v.Title,
+			CommentCount:  video.CommentCount,
+			CoverURL:      video.CoverUrl,
+			FavoriteCount: video.FavoriteCount,
+			ID:            video.ID,
+			IsFavorite:    isFavorite,
+			PlayURL:       video.PlayUrl,
+			Title:         video.Title,
 		}
 		videosList = append(videosList, video)
 	}
